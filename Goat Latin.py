25b7b744d4893941e5a481a25c5acd8b00363200
singhdavinderpa1 <singhdavinderpal007@gmail.com>
2022-01-01 17:00:03 +0100

    Goat Latin solution

---
+++ Goat Latin.py
@@ -0,0 +1,23 @@
+class Solution:
+    def toGoatLatin(self, sentence: str) -> str:
+        vowel = ['a', 'e', 'i', 'o' , 'u', "A" , "E" , "I", "O", "U"]
+        sentence = [i for i in sentence.split()]
+        print(sentence)
+        for i in range(len(sentence)):
+            print(sentence[i])
+            if sentence[i][0] in vowel:
+                sentence[i] = sentence[i]+"ma" + (i+1)*"a"
+            else:
+                sentence[i] = sentence[i][1:] + sentence[i][0] + "ma" + (i+1)*"a"
+
+
+        return  (" ".join(sentence))
+
+
+
+obj = Solution()
+
+
+str = "Each word consists of lowercase and uppercase letters only"
+
+print(obj.toGoatLatin(str))
